03667b1a9b981e46030bd8d39cad39a4621c72b0
Jimmy Kittiyachavalit <jimmy@users.noreply.github.com>
2016-01-18 20:59:56 -0800

    Create basic working model
    This is a first draft of the app. It allows people to input a total bill
    amount and choose from a few predefined percentages.

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipAmount = 0.0
+        var totalAmount = 0.0
+        let tipPercentages = [0.15, 0.18, 0.20, 0.25]
+        let tipPercent = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        if let billText = billField.text {
+            if let billAmount = Double(billText) {
+                tipAmount = billAmount * tipPercent
+                totalAmount = billAmount + tipAmount
+            }
+        }
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
-
